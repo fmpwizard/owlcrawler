99e8691ff0387d38b8ef25ba254b5d33299cbf52
Diego Medina <diego@fmpwizard.com>
2015-03-01 05:49:17 -0500

    deleted more dead code

--- parse/parse.go
+++ parse/parse.go
@@ -25,38 +25,6 @@ type ExtractedLinks struct {
 
 type URLFetchChecker func(url string) bool
 
-/*func ExtractText(payload string) PageStructure {
-	var page PageStructure
-
-	nodes, err := html.Parse(strings.NewReader(payload))
-	if err != nil {
-		log.Printf("Error parsing html, got: %+v\n", err)
-	}
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode {
-			if n.Data == "title" && n.FirstChild != nil {
-				page.Title = n.FirstChild.Data
-			} else if n.Data == "h1" {
-				page.H1 = append(page.H1, n.FirstChild.Data)
-			} else if n.Data == "h2" {
-				page.H2 = append(page.H2, n.FirstChild.Data)
-			} else if n.Data == "h3" {
-				page.H3 = append(page.H3, n.FirstChild.Data)
-			} else if n.Data == "h4" {
-				page.H4 = append(page.H4, n.FirstChild.Data)
-			} else if n.FirstChild != nil && strings.TrimSpace(n.FirstChild.Data) != "" {
-				page.Text = append(page.Text, n.FirstChild.Data)
-			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(nodes)
-	return page
-}*/
-
 func ExtractText(payload string) PageStructure {
 	var page PageStructure
 
