4e2e15cd3eafcc669959d7e70bafd46356832dc1
Diego Medina <diego@fmpwizard.com>
2015-09-05 22:48:14 -0400

    Ansible scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 go build  -tags=fetcherSched -o owlcrawler-scheduler owlcrawler_scheduler.go && \
-go build  -tags=fetcherExec -o owlcrawler-executor-fetcher owlcrawler_executor_fetcher.go && \
-go build  -tags=extractorExec -o owlcrawler-executor-extractor owlcrawler_executor_extractor.go 
+go build  -tags=fetcherExec -o owlcrawler-fetcher owlcrawler_executor_fetcher.go && \
+go build  -tags=extractorExec -o owlcrawler-extractor owlcrawler_executor_extractor.go 
